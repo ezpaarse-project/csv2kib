e7fd62a8e2102009363dce740c3c1b3b7ffd2401
Yannick Schurter <yannick.schurter@gmail.com>
2021-08-11 16:03:28 +0200

    first basic version

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <textarea
+      v-model="csv"
+      cols="50"
+      rows="20"
+    />
+    <textarea
+      :value="kibanaQuery"
+      cols="50"
+      rows="20"
+    />
+    <button @click="updateKibanaFilter">Pool!</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Papa from 'papaparse';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+  },
+  data() {
+    return {
+      csv: '',
+      column: 'umr',
+      kibanaQuery: '',
+      kibanaField: 'unit',
+    };
+  },
+  methods: {
+    updateKibanaFilter() {
+      if (!this.csv.trim() || !this.kibanaField.trim()) {
+        this.kibanaQuery = '';
+        return;
+      }
+
+      Papa.parse(this.csv, {
+        delimiter: ';',
+        header: true,
+        complete: ({ data: rows, errors }) => {
+          if (errors.length > 1) {
+            errors.forEach((e) => console.error(e));
+          }
+
+          const should = rows
+            .filter((row) => row[this.column])
+            .map((row) => ({
+              match_phrase: {
+                [this.kibanaField]: row[this.column],
+              },
+            }));
+
+          const kibanaQuery = {
+            query: {
+              bool: {
+                minimum_should_match: 1,
+                should,
+              },
+            },
+          };
+
+          this.kibanaQuery = JSON.stringify(kibanaQuery, null, 2);
+        },
+      });
+    },
   },
 };
 </script>
