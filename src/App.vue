c5e7cc8ee4ad5476f6dd5dc2fa81a705a7d055ef
Yannick Schurter <yannick.schurter@gmail.com>
2021-08-13 10:06:57 +0200

    feat: introduce PrimeVue and add some options

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,58 @@
 <template>
-  <div id="app">
-    <textarea
-      v-model="csv"
-      cols="50"
-      rows="20"
-    />
-    <textarea
-      :value="kibanaQuery"
-      cols="50"
-      rows="20"
-    />
-    <button @click="updateKibanaFilter">Pool!</button>
+  <div id="app" class="flex justify-content-center">
+    <div class="flex-grow-1 flex flex-column m-5" style="max-width: 800px">
+      <p-fieldset legend="Fichier source" class="border-dashed border-2">
+        <p class="text-center">
+          <p-button
+            label="Sélectionner un fichier CSV"
+            icon="pi pi-file"
+            class="p-button-lg"
+            @click="$refs.fileInput.click()"
+          />
+          <input
+            ref="fileInput"
+            type="file"
+            class="hidden"
+            @change="onFileSelect"
+          />
+        </p>
+      </p-fieldset>
+
+      <p-fieldset legend="Options" class="mt-4">
+        <div class="formgrid grid">
+            <div class="field col">
+              <label for="csv-headers">Colonne contenant les valeurs</label>
+
+              <p-dropdown
+                id="csv-headers"
+                v-model="selectedColumn"
+                :options="csvHeaders"
+                :disabled="csvHeaders.length === 0"
+                class="w-full"
+              />
+            </div>
+
+            <div class="field col">
+              <label for="kibana-field">Champ Kibana à filtrer</label>
+
+              <p-input-text
+                id="kibana-field"
+                v-model="kibanaField"
+                type="text"
+                class="w-full"
+              />
+            </div>
+        </div>
+      </p-fieldset>
+
+      <p-fieldset legend="Query Kibana" class="mt-4">
+        <p-textarea
+          class="w-full h-full"
+          :value="kibanaQuery"
+          rows="20"
+        />
+      </p-fieldset>
+    </div>
   </div>
 </template>
 
@@ -23,59 +65,65 @@ export default {
   },
   data() {
     return {
-      csv: '',
-      column: 'umr',
-      kibanaQuery: '',
-      kibanaField: 'unit',
+      csvHeaders: [],
+      csvRows: [],
+      selectedColumn: null,
+      kibanaField: '',
     };
   },
-  methods: {
-    updateKibanaFilter() {
-      if (!this.csv.trim() || !this.kibanaField.trim()) {
-        this.kibanaQuery = '';
-        return;
-      }
+  computed: {
+    kibanaQuery() {
+      if (!this.csvRows.length === 0) { return ''; }
+      if (!this.selectedColumn) { return ''; }
+      if (!this.kibanaField.trim()) { return ''; }
 
-      Papa.parse(this.csv, {
+      const should = this.csvRows
+        .filter((row) => row[this.selectedColumn])
+        .map((row) => ({
+          match_phrase: {
+            [this.kibanaField]: row[this.selectedColumn],
+          },
+        }));
+
+      const kibanaQuery = {
+        query: {
+          bool: {
+            minimum_should_match: 1,
+            should,
+          },
+        },
+      };
+
+      return JSON.stringify(kibanaQuery, null, 2);
+    },
+  },
+  methods: {
+    onFileSelect(event) {
+      const files = event?.target?.files;
+      const file = files && files[0];
+
+      if (!file) { return; }
+
+      this.csvHeaders = [];
+      this.csvRows = [];
+      this.selectedColumn = null;
+
+      Papa.parse(file, {
         delimiter: ';',
         header: true,
+        transformHeader: (header) => {
+          this.csvHeaders.push(header);
+          return header;
+        },
         complete: ({ data: rows, errors }) => {
           if (errors.length > 1) {
             errors.forEach((e) => console.error(e));
           }
-
-          const should = rows
-            .filter((row) => row[this.column])
-            .map((row) => ({
-              match_phrase: {
-                [this.kibanaField]: row[this.column],
-              },
-            }));
-
-          const kibanaQuery = {
-            query: {
-              bool: {
-                minimum_should_match: 1,
-                should,
-              },
-            },
-          };
-
-          this.kibanaQuery = JSON.stringify(kibanaQuery, null, 2);
+          this.csvRows = rows;
+          [this.selectedColumn] = this.csvHeaders;
         },
       });
     },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import Textarea from 'primevue/textarea';
+import Fieldset from 'primevue/fieldset';
+import FileUpload from 'primevue/fileupload';
+import Dropdown from 'primevue/dropdown';
+import InputText from 'primevue/inputtext';
+
+import 'primeflex/primeflex.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('p-button', Button);
+app.component('p-textarea', Textarea);
+app.component('p-fieldset', Fieldset);
+app.component('p-file-upload', FileUpload);
+app.component('p-dropdown', Dropdown);
+app.component('p-input-text', InputText);
+
+app.mount('#app');
